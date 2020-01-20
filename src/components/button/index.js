f7028b6c728412a23063220792885731e67c3e50
Frank <1305679132@qq.com>
2020-01-20 20:33:54 +0800

    'a'

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,33 +1,20 @@
 import React from 'react'
+import './index.css'
 
 function Button (props) {
-    const fButton = {
-        width: '70px',
-        height: '30px',
-        backgroundColor: 'skyblue',
-        outLine: 'none',
-        border: 'none',
-        borderRadius: '4px',
-        color: 'white',
-        letterSpacing: '2px',
-        cursor: 'pointer'
-    }
     return (
-        <div>
-            <button
-                style={fButton}
-                onClick={() => {
-                    props.onClick()
-                }}
-                className={
-                    "f-button " + props.className
-                }
-            >
-                {
-                    props.children === '' ? '按钮' : props.children
-                }
-            </button>
-        </div>
+        <button
+            onClick={() => {
+                props.onClick()
+            }}
+            className={
+                "f-button " + props.className
+            }
+        >
+            {
+                props.children === '' ? '按钮' : props.children
+            }
+        </button>
     )
 }
 
